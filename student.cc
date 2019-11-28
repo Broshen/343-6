0eb0fda3d1b0ffd083c104a40c160b4a05f2449f
j272zhou <j272zhou@student.cs.uwaterloo.ca>
2019-11-27 19:45:29 -0500

    Add student starter code

---
+++ student.cc
@@ -0,0 +1,14 @@
+#include "student.h"
+#include "MPRNG.h"
+extern MPRNG mprng;
+Student::Student( Printer & prt, NameServer & nameServer, WATCardOffice & cardOffice, Groupoff & groupoff,
+  			 unsigned int id, unsigned int maxPurchases ) : prt(prt), nameServer(nameServer), cardOffice(cardOffice), groupoff(groupoff), id(id), maxPurchases(maxPurchases) {}
+
+
+void Student::main() {
+	unsigned int toPurchase = mprng(1, maxPurchases);
+	unsigned int favFlavour = mprng(3);
+
+	yield(mprng(1, 10));
+
+}
--- student.h
+++ student.h
@@ -1,7 +1,19 @@
 #ifndef __STUDENT__
 #define __STUDENT__
 
+#include "printer.h"
+#include "nameserver.h"
+#include "watcard_office.h"
+#include "groupoff.h"
+
 _Task Student {
+	Printer &prt;
+	NameServer &nameServer;
+	WATCardOffice &cardOffice;
+	Groupoff &groupoff;
+	unsigned int id;
+	unsigned int maxPurchases;
+	
 	void main();
   public:
 	Student( Printer & prt, NameServer & nameServer, WATCardOffice & cardOffice, Groupoff & groupoff,
