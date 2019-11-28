ec865866aa97f57eab8f96c751754ab168b4514c
Boshen Cui <boshen.cui@gmail.com>
2019-11-27 19:37:59 -0500

    printer first pass

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 using namespace std;					// direct access to std
 #include <cstdlib>					// exit
 #include "config.h"
+#include "printer.h"
 
 int main( int argc, char * argv[] ) {
     // MUST BE INT (NOT UNSIGNED) TO CORRECTLY TEST FOR NEGATIVE VALUES
@@ -31,5 +32,7 @@ int main( int argc, char * argv[] ) {
     //cout << "size " << size << " code " << code << endl;
     ConfigParms configParms;
     processConfigFile(filename.c_str(), configParms);
+
+    Printer printer(configParms.numStudents, configParms.numVendingMachines, configParms.numCouriers); 
     
 } // main
